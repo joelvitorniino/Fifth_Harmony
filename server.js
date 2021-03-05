140d502c426629a7efce85bc979b71f4b44d3c93
joelvitorniino <joelprofessor13@gmail.com>
2021-03-04 22:30:26 -0300

    second commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/src/html/'));
+app.use(express.static(__dirname + './src/html/'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(__dirname + '/src/html/index.html');
+    res.sendFile(__dirname + './src/html/index.html');
 });
 
 app.listen(PORT, () => {
