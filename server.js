db3c169ed3e605f08e91b972b43d675986d802f9
joelvitorniino <joelprofessor13@gmail.com>
2021-03-04 22:25:02 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/src/html/'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/src/html/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciando na porta: ${PORT}`);
+});
